f6154dc100fc307b9ca3842d0e5d4cbc404d0c23
Pabasara Jayamanne <127324149+PabasaraJay@users.noreply.github.com>
2023-05-08 16:53:34 +0530

    Add files via upload

---
+++ Lab5/ex2.py
@@ -0,0 +1,10 @@
+#Finding the largest number between two numbers
+
+print("Enter two numbers")
+numOne = int(input())
+numTwo = int(input())
+
+if numOne > numTwo:
+    print("\n Largest Number =",numOne)
+else:
+    print("\n Largest Number =",numTwo)
