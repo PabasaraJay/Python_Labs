888d113940a3275d9a7d83bf529776ec3f5492dd
Pabasara Jayamanne <127324149+PabasaraJay@users.noreply.github.com>
2023-05-08 17:15:54 +0530

    done

---
+++ Lab5/ex4.py
@@ -0,0 +1,8 @@
+#python programme to find odd or even number
+
+num = int(input("Enter the number to test whether it is odd or even: "))
+
+if(num % 2)==0:
+    print("Number is even")
+else:
+    print("Number id odd")
