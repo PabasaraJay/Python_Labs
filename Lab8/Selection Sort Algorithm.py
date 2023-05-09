eea74f5fe82330070f2c70d28ace2af5b0dd6829
Pabasara Jayamanne <127324149+PabasaraJay@users.noreply.github.com>
2023-05-09 10:58:49 +0530

    part1

---
+++ Lab8/Selection Sort Algorithm.py
@@ -0,0 +1,18 @@
+A= []
+for v in range(8):
+    A.append(int(input('Enter a Number : ')))
+
+print(A)
+
+def selectionSort(A):
+    n = len(A)
+    for j in range(0,n-1):
+        smallest = j
+        for i in range(j+1,n):
+            if A[i]< A[smallest]:
+                smallest = i
+            A[j],A[smallest]= A[smallest],A[j]
+
+selectionSort(A)
+print('Sorted Array : ',A)
+        
